f1c184684869a7e2a3adfed32c1f69c2d5a67e3b
caspase7 <98258369+caspase7@users.noreply.github.com>
2022-02-11 21:12:47 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -341,10 +341,10 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '试着好好练一下？';
-    if (score <= 99) return 'TCL';
-    if (score <= 149) return 'TQL';
-    if (score <= 199) return '您';
+    if (score <= 49) return 'cyf1';
+    if (score <= 99) return 'cyf';
+    if (score <= 149) return 'cyf';
+    if (score <= 199) return 'cyf';
     return '人？';
 }
 
